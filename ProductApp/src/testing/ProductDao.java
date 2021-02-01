fa0fd65586d06c3e303d284297f5744afaff2b1a
Shivansh Keshari <eshukeshari@gmail.com>
2021-02-01 11:56:43 +0530

    Assignment Update

--- ProductApp/src/testing/ProductDao.java
+++ ProductApp/src/testing/ProductDao.java
@@ -2,16 +2,23 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import dao.ProductDao;
+import bean.Product;
+
 public class ProductDao {
+	static ProductDao pd;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		pd=new ProductDao();
 	}
 
 	@AfterClass
@@ -26,9 +33,38 @@ public class ProductDao {
 	public void tearDown() throws Exception {
 	}
 
+	//@Test
+	public void testDisplayallproduct() {
+		List<Product> listofproduct = pd.Displayallproduct();
+		assertEquals(3,listofproduct.size());
+	}
+
+	//@Test
+	public void testAddproduct() {
+		Product p=new Product();
+		p.setPid(100);
+		p.setPname("computer");
+		p.setPrice(20000.00);
+		int res=pd.Addproduct(p);
+		int msg=1;
+		assertEquals(msg,res);
+	}
+
+	//@Test
+	public void testUpdateproduct() {
+		Product p=new Product();
+		p.setPid(100);
+		p.setPrice(20000.00);
+		int res	= pd.Updateproduct(p);
+		assertEquals(1, res);
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDeleteProduct() {
+		Product p=new Product();
+		p.setPid(100);
+		int res=pd.DeleteProduct(p);
+		assertEquals(1,res);
 	}
 
 }
